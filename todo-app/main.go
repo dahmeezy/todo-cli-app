7d7a5437fb5103fb3de475bde24e4ee2ec9ed561
zaiyusuf <zainaby1008@gmail.com>
2026-07-20 16:05:13 +0100

    proper naming of variabled

--- todo-app/main.go
+++ todo-app/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"text/tabwriter"
@@ -35,15 +34,13 @@ func main() {
 	var plan string
 	var todoList []Todo
 
-	res := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+	result := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
 
 	fmt.Println("Welcome To TODO-APP!")
 	fmt.Println("Do you want to?")
 	for option != 3 {
 
-		fmt.Println("1. View your recorded tasks and their status.")
-		fmt.Println("2. Add a new task")
-		fmt.Println("3. Exit")
+		printMenu()
 
 		fmt.Scanln(&option)
 
@@ -51,34 +48,15 @@ func main() {
 		case 1:
 			if len(todoList) == 0 {
 				fmt.Println("You have nothing in your todo list, Try adding a new task!")
-				
 
 			} else {
-				fmt.Fprintf(res, "ID\tTASK\tSTATUS\n")
-				fmt.Fprintln(res, "--\t----\t------")
-
-				for _, items := range todoList {
-
-					fmt.Fprintf(res, "%d\t%s\t%t\n", items.ID, items.Task, items.Status)
-				}
-				res.Flush()
+				viewTask(todoList, result)
 				fmt.Println()
 			}
 		case 2:
 			fmt.Println("Note down a task")
-			// create a scanner that listens directly to standard input
-			scanner := bufio.NewScanner(os.Stdin)
-			// pause the CLI program and waits for the user to type something and press Enter
-			scanner.Scan()
-			// save the user input in a variable
-			plan = scanner.Text()
+			todoList = addNewTask(idcount, plan, todoList)
 			idcount++
-			todo := Todo{
-				ID:     idcount,
-				Task:   plan,
-				Status: false,
-			}
-			todoList = append(todoList, todo)
 			fmt.Println("Your task has been added!")
 			fmt.Println()
 		case 3:
--- todo-app/todo.go
+++ todo-app/todo.go
@@ -1,2 +1,42 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"text/tabwriter"
+)
+
+func addNewTask(idcount int, plan string, todoList []Todo) []Todo{
+	// create a scanner that listens directly to standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	// pause the CLI program and waits for the user to type something and press Enter
+	scanner.Scan()
+	// save the user input in a variable
+	plan = scanner.Text()
+	idcount++
+	todo := Todo{
+		ID:     idcount,
+		Task:   plan,
+		Status: false,
+	}
+	todoList = append(todoList, todo)
+	return todoList
+}
+
+func viewTask(todoList []Todo, result *tabwriter.Writer) {
+	fmt.Fprintf(result, "ID\tTASK\tSTATUS\n")
+	fmt.Fprintln(result, "--\t----\t------")
+
+	for _, items := range todoList {
+
+		fmt.Fprintf(result, "%d\t%s\t%t\n", items.ID, items.Task, items.Status)
+	}
+	result.Flush()
+}
+
+func printMenu() {
+	fmt.Println("1. View your recorded tasks and their status.")
+	fmt.Println("2. Add a new task")
+	fmt.Println("3. Exit")
+}
